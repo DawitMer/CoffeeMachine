effe482a4f3a17c354052d30b1d97d09b657f44d
dave <dawitymersha@gmail.com>
2025-01-05 19:54:50 -0500

    small change

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ RESOURCE = {"water": 300, "milk": 200, "coffee": 100, "money": 0}
 def report():
     """DISPLAY THE STORE INFORMATION"""
     for stock in RESOURCE:
-        print(f"{stock.title()}: {RESOURCE[stock]}" )
+        if stock == "water" or stock == "milk":
+            print(f"{stock.title()}: {RESOURCE[stock]}ml")
+        elif stock == "coffee":
+            print(f"{stock.title()}: {RESOURCE[stock]}gm")
+        else:
+            print(f"{stock.title()}: ${RESOURCE[stock]}")
+
 
 
 #TODO: 2 when ordered check resource
-def check_stock(customer_order):
-    """Take the customer order as an input (Customer_order) and check if te resource is available return True or Else """
-    for menu_info in MENU[customer_order]["ingredients"]:
-        if RESOURCE[menu_info] < MENU[customer_order]["ingredients"][menu_info]:
+def check_stock(drink):
+    """Take the drink as an input (Customer_order) and check if te resource is available return True or Else """
+    for menu_info in drink:
+        if RESOURCE[menu_info] < drink[menu_info]:
             print(f"Sorry there is not enough {menu_info}.")
             return False
     return True
@@ -24,38 +30,38 @@ def check_stock(customer_order):
 
 #TODO: 3 check the coin paid and return change
 
-def price_check(customer_order, quarter, dime, nickle, pennie):
+def is_transaction_successful(drink_price, quarter, dime, nickle, pennie):
     """CHECK THE CUSTOMER ORDER WITH THE PRICE AND GIVE BACK CHANGE IF
-    THERE IS AND WILL ALSO RETURN TRUE IS THE IF THE MONEY PAID IS SUFFICIENT BY THE CUSTOMER"""
+    THERE IS AND WILL ALSO RETURN TRUE IF THE MONEY PAID IS SUFFICIENT BY THE CUSTOMER"""
     total_cash = (quarter * 0.25) + (dime * 0.10) + (nickle * 0.05) + (pennie * 0.01)
-    product_price = MENU[customer_order]["cost"]
-    if product_price < total_cash:
-        change = round(total_cash - product_price, 2)
+    if drink_price < total_cash:
+        change = round(total_cash - drink_price, 2)
         print(f"Here is ${change} in change.")
 
         return True
     return False
 
 
-def payment(customer_order):
-    """WILL TAKE THE CUSTOMER ORDER AND TAKE INPUT FROM THE CUSTOMER TO FORWARD IT
-     TO FUNCTION price_check(customer_order, quarter, dime, nickle, pennie)
-     IT ALSO RETURNS THE price_check(customer_order, quarter, dime, nickle, pennie)
+def payment(drink_price):
+    """WILL TAKE THE drink price AND TAKE INPUT FROM THE CUSTOMER TO FORWARD IT
+     TO FUNCTION price_check(drink_price, quarter, dime, nickle, pennie)
+     IT ALSO RETURNS THE price_check(drink_price, quarter, dime, nickle, pennie)
      """
+    print("Please insert coins.")
     quarters = int(input("How many quarters?: "))
     dime = int(input("How many dimes?: "))
     nickle = int(input("How many nickle?: "))
     pennies = int(input("How many nickle?: "))
-    return price_check (customer_order, quarters, dime, nickle, pennies)
+    return is_transaction_successful (drink_price, quarters, dime, nickle, pennies)
 
 
 
 #TODO: 4 update the resource
-def update_resource(customer_order):
+def update_resource(drink_resource, customer_payment):
     """TAKE THE CUSTOMER ORDER AND UPDATE THE STOCK ACCORDINGLY AND PUT THE CASH IN REGISTER"""
-    for menu_info in MENU[customer_order]["ingredients"]:
-        RESOURCE[menu_info] -= MENU[customer_order]["ingredients"][menu_info]
-    RESOURCE["money"] += MENU[customer_order]["cost"]
+    for menu_info in drink_resource:
+        RESOURCE[menu_info] -= drink_resource[menu_info]
+    RESOURCE["money"] += customer_payment
 
 
 
@@ -64,16 +70,19 @@ def coffee_machine():
     is_on = True
     while is_on:
         order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
         if order == "report":
             report()
         elif order == "off":
             is_on = False
         else:
             #check stock
-            if check_stock(order):
+            drink_resource = MENU[order]["ingredients"]
+            if check_stock(drink_resource):
                 #check the money
-                if payment(order):
-                    update_resource(order)
+                price = MENU[order]["cost"]
+                if payment(price):
+                    update_resource(drink_resource, price)
                     # TODO: 5 give the the order to the customer
                     print("Here is your latter ☕️ Enjoy!")
                 else:
